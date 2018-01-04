b06f9a4fbc6d1d4b2814ab35464b6a657a4f4230
kechyap <kechyap@gmail.com>
2018-01-04 14:31:33 +0700

    gradient

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 // Import things we made ourselves
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import logo_white from './icon/logo_white.svg'
 
 import icon_email from './icon/icon_email.svg';
 import icon_facebook from './icon/icon_facebook.svg';
@@ -28,37 +29,40 @@ class App extends React.Component {
           date:        'february 2018',
           title:       'SEAM CENTER',
           text:        'co-working and co-living space ',
-          color:       'rgba(250, 189, 52)',
+          color:       'rgba(255, 255, 255, 0.9)',
           photoAmount: ['01','02','03','04','05']
         },
         {
           folderName:  'PF_02_POPS',
           date:        '2017',
-          title:       'P.O.P.S FORUM',
+          title:       'P O P S FORUM',
           text:        'Private Owned Public Space',
-          color:       'rgba(250, 189, 52)',
+          color:       'rgba(255, 255, 255, 0.9)',
           photoAmount: ['01','02','03','04']
         },
         {
           folderName:  'PF_03_Grill',
           date:        'august 2017',
           title:       'GRILL BAG',
-          text:        'restaurant, social issue',
-          color:       'rgba(250, 189, 52)',
+          text:        'social issue, restaurant',
+          color:       'rgba(255, 255, 255, 0.9)',
           photoAmount: ['01','02','03','04']
         }
       ]
     }
   }
 
+
+
   // Rendering this App Component
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Art and Architecture Atelier </h1>
-          <p className="App-intro" font-size="0.5rem">
+          <img src={logo_white} className="App-logo" alt="logo" />
+          <h1 className="App-title"> AAAtelier</h1>
+          <h4 className="App-sub">  Art and Architecture Atelier  </h4>
+          <p className="App-intro">
             #human #future #lifestyle #brading #space_identity <br/>
             #social_issue #community #common_space #co-living #co-working <br/>
             #environmental_issue #upcycle #urban_issue #public_space #POPS
@@ -77,11 +81,11 @@ class App extends React.Component {
             {this.state.projects.map((x) =>
               <Col xs={12} sm={6} md={4} xl={3}>
 
-                <div style={{color: x.color, paddingTop:"2rem"}}>
+                <h4 style={{color: x.color, marginTop:"0.9rem" , marginBottom:"0rem"}}>
                   {x.title}
-                </div>
+                </h4>
 
-                <div style={{color:"rgb(128, 107, 80)", fontSize: "0.7rem" , paddingBottom: "0.3rem"}}>
+                <div style={{color:"rgba(255, 255, 255, 0.8)", fontSize: "0.75rem" , paddingBottom: "0.3rem"}}>
                   {x.text}
                 </div>
 
