cb222a70de15c0378f1968daa78a364f150efc34
kechyap <kechyap@gmail.com>
2018-01-05 23:12:06 +0700

    new one

--- src/Aaatelier.js
+++ src/Aaatelier.js
@@ -50,11 +50,23 @@ class Aaatelier extends React.Component {
       <Grid fluid>
         <Row>
           <Col xs={12} md={4}>
-            <img src={require('./icon/logo_white.svg')} style={{width: '4rem'}} />
+            <img src={require('./icon/logo_white.svg')} style={{width: '5rem'}} />
           </Col>
           <Col xs={12} md={8}>
-            <h1>AAAtelier</h1>
-            <h4>Art and Architecture Atelier</h4>
+            <h2>AAAtelier</h2>
+            <p>Art and Architecture Atelier</p>
+            <div>
+              <h4>문제 해결.</h4>
+              <p>급속한 도시화로 인한 문제 및 환경 문제, 사회이슈들을 개선하기 위한 제안과 그에 따른 합리적인 디자인을 제안드립니다.</p>
+
+              <h4> 커뮤니티.</h4>
+              <p>크리에이티브한 창작자들을 위한 쉐어 하우스, 코워킹 스페이스, 창작자 마을을 구축하기 위한 커뮤니티 공유 공간을 제안 드립니다.</p>
+
+              <h4>통합 디자인 솔루션. </h4>
+              <p>도시, 건축, 인테리어, 가구, 소품, 그래픽 디자인을 통합하여 다양한 스케일에서 일관된 해결책을 찾고자 하는 Art and Architecture Atelier입니다.</p>
+
+
+            </div>
           </Col>
         </Row>
         <Row>
@@ -76,6 +88,7 @@ class Aaatelier extends React.Component {
         {this.state.projects.map((project) =>
           <div>
             <hr />
+            
             <Row>
               <Col xs={12} md={4}>
                 <h4 style={{color: project.color}}>
@@ -85,6 +98,7 @@ class Aaatelier extends React.Component {
                   {project.text}
                 </p>
               </Col>
+
               <Col xs={12} md={8}>
                 <Row>
                   {project.photos.map((photo) =>
@@ -93,10 +107,12 @@ class Aaatelier extends React.Component {
                       <br />
                       <br />
                     </Col>
+
                   )}
                 </Row>
               </Col>
             </Row>
+
           </div>
         )}
       </Grid>
