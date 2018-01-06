5ddbe81d92aaf68b09d1f76b7062111f2e2d2110
kechyap <kechyap@gmail.com>
2018-01-06 21:07:01 +0700

    Navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './index.css'
 
-import logo from '../../images/logo.svg'
+import simple_logo from './simple_logo.svg'
 
-const Navigation = () => (
-  <div>
-    <img src={logo} style={{width: '5rem'}} />
-    <h2>AAAtelier</h2>
-    <p>Art and Architecture Atelier</p>
-    <Link to='/'>Home</Link>
-    <Link to='/about'>About</Link>
-    <Link to='/seam'>Seam</Link>
-    <Link to='/upcycle'>Upcycle</Link>
-    <Link to='/grill'>Grill</Link>
-    <Link to='/pops'>Pops</Link>
+export const Navigation = () => (
+  <div className='navbar-wrapper'>
+    <div className='navbar'>
+      <img className='simple_logo' src={simple_logo} />
+      <Link to='/'>Projects</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/seam'>Seam</Link>
+      <Link to='/upcycle'>Upcycle</Link>
+      <Link to='/grill'>Grill</Link>
+      <Link to='/pops'>Pops</Link>
+    </div>
   </div>
 )
 
