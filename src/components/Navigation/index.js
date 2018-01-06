200a55db2e77d27a45436afda54972a4e98eb8ed
kechyap <kechyap@gmail.com>
2018-01-06 19:57:30 +0700

    Logo in Navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './index.css'
 
+import logo from '../../images/logo.svg'
+
 const Navigation = () => (
   <div>
+    <img src={logo} style={{width: '5rem'}} />
+    <h2>AAAtelier</h2>
+    <p>Art and Architecture Atelier</p>
     <Link to='/'>Home</Link>
     <Link to='/about'>About</Link>
     <Link to='/seam'>Seam</Link>
