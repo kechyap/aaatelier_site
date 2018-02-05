768fadeaad3e74917df4ed93eecd15ea21e58cb4
kechyap <kechyap@gmail.com>
2018-02-05 22:43:27 +0700

    master branch

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -29,13 +29,24 @@ const About = () => (
     <div className='ab0'>
       <Grid fluid>
         <Row>
+          <Col xs={4} sm={4} md={3} >
+            <h4> TOTAL DESIGN SOLUTION </h4>
+          </Col>
+
+          <Col xs={8} sm={8} md={7}>
+            <div className='aboutText'>
+            도시, 건축, 인테리어, 가구, 소품, 그래픽 디자인을 통합하여 다양한 스케일에서 일관된 해결책을 찾고자 하는 Art and Architecture Atelier입니다.
+          </div>
+          </Col>
+
+
           <Col xs={4} sm={4} md={3}>
             <h4> SOCIAL ISSUE </h4>
           </Col>
 
           <Col xs={8} sm={8} md={7}>
             <div className='aboutText'>
-            급속한 도시화로 인한 문제 및 환경 문제, 사회 이슈들을 개선하기 위한 제안과 그에 따른 합리적인 디자인을 제안드립니다.
+            AAAtelier는 급속한 도시화로 인한 문제 및 환경 문제, 사회 이슈들을 개선하기 위해 고민하고, 그에 따른 합리적인 디자인을 제안드립니다.
           </div>
           </Col>
 
@@ -49,15 +60,7 @@ const About = () => (
           </div>
           </Col>
 
-          <Col xs={4} sm={4} md={3} >
-            <h4> TOTAL DESIGN SOLUTION </h4>
-          </Col>
 
-          <Col xs={8} sm={8} md={7}>
-            <div className='aboutText'>
-            도시, 건축, 인테리어, 가구, 소품, 그래픽 디자인을 통합하여 다양한 스케일에서 일관된 해결책을 찾고자 하는 Art and Architecture Atelier입니다.
-          </div>
-          </Col>
         </Row>
       </Grid>
     </div>
