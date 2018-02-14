96f2d4547b60b697343e2dcdeefb9fe06cfd9725
Superpencil <pascal@superpencil.com>
2018-02-14 13:58:11 +0700

    <ScrollToTop /> googleAnalytics() <Error /> max-width for <Grid />

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route, Redirect, Switch } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
+
 import './index.css'
+import googleAnalytics from './googleAnalytics'
+import ScrollToTop from './ScrollToTop'
 
 import Navigation from '../Navigation/index.js'
 import Home from '../Home/index.js'
@@ -11,18 +15,46 @@ import Upcycle from '../Upcycle/index.js'
 import Grill from '../Grill/index.js'
 import Pops from '../Pops/index.js'
 
-const App = () => (
-  <Router>
-    <div>
+// Error component
+//   Used in the Routes component
+const Error = () => (
+  <div>
+    <h1>Error</h1>
+  </div>
+)
 
+// Routes component
+//   max-width around whole site
+//   Navigation is always shown
+//   Switch changes component based on route in url bar
+//   If nothing matches, show the Error component
+const Routes = () => (
+  <div>
+    <Navigation />
+    <Switch>
       <Route exact path='/' component={Home} />
-      <Navigation />
       <Route path='/about' component={About} />
       <Route path='/seam' component={Home} />
       <Route path='/upcycle' component={Home} />
       <Route path='/grill' component={Home} />
       <Route path='/pops' component={Home} />
-    </div>
+      <Route component={Error} />
+    </Switch>
+  </div>
+)
+
+// Makes a fake 'browser history'
+//   so Google Analytics can follow the user
+const history = createHistory()
+
+// Actual App component
+//   Also ScrollToTop when user changes pages
+//   Then inside is the Routes component inside of googleAnalytics function
+const App = () => (
+  <Router history={history}>
+    <ScrollToTop>
+      <Route component={googleAnalytics(Routes)} />
+    </ScrollToTop>
   </Router>
 )
 
