bf50d98ce4f3bb785f611be552bfb4b4a380401d
kechyap <kechyap@gmail.com>
2018-02-16 17:46:38 +0700

    Remove App as a separate component, too confusing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
-// import npm files
+// import npm files & components
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'normalize.css'
 
-// import local files
-import App from './components/App'
+// import local files & components
+import Navigation from './components/Navigation'
+import Home from './components/Home'
+import About from './components/About'
+import Seam from './components/Seam'
+import Upcycle from './components/Upcycle'
+import Grill from './components/Grill'
+import Pops from './components/Pops'
+import Error from './components/Error'
 import './index.css'
+
+// make App component
+const App = () => (
+  <Router>
+    <div className='App'>
+      <Navigation />
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/seam'>
+          <Seam />
+        </Route>
+        <Route path='/upcycle'>
+          <Upcycle />
+        </Route>
+        <Route path='/grill'>
+          <Grill />
+        </Route>
+        <Route path='/pops'>
+          <Pops />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+)
 
 // render App component on <div id='root'></div> in /public/index.html
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// no export because of render
+// no export because of .render()
