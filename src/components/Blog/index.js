8a767e9c7a34382c883337b62a1c28995d302cc1
kechyap <kechyap@gmail.com>
2018-04-06 18:56:14 +0800

    blog writing1

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,39 +1,38 @@
 // import npm files & components
 import React from 'react'
 import { Grid, Row, Col } from 'react-flexbox-grid'
+import { Link } from 'react-router-dom'
 
 // import local files & components
+import Writing1 from './blogWritings/Writing1'
+import Writing2 from './blogWritings/Writing2'
 import './index.css'
-import Writing1 from './blogWritings/Writing1/index.js'
-import Writing2 from './blogWritings/Writing2/index.js'
-import Foot from '../Foot'
 
-// make data
-// make Upcycle component
+// make App component
 const Blog = () => (
   <div className='Blog'>
+
     <Grid fluid>
       <Row>
-        <Col xs={12}>
-          <div className='py-5 px-1'>
-            <h1> CREATIVE SPACE <br />
-                 IN THE WORLD!!!
+        <Col xs={12} sm={5}>
+          <div className='py-3'>
+            <br /><br /><br />
+            <h1>
+              Creative Space <br />
+              over the world
             </h1>
           </div>
         </Col>
       </Row>
     </Grid>
 
-    <div className='py-1'>
+    <div>
       <Writing1 />
     </div>
 
-    <div className='py-1'>
+    <div>
       <Writing2 />
     </div>
-
-    <hr />
-    <Foot />
   </div>
 )
 
