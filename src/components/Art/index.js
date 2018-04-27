96f5b5e6c569dfbef9aece326d41c833355761fc
kechyap <kechyap@gmail.com>
2018-04-27 18:14:30 +0800

    art changes

--- src/components/Art/index.js
+++ src/components/Art/index.js
@@ -22,46 +22,53 @@ const Art = () => (
           </h1>
         </Col>
 
-        <Col xs={12} sm={5} md={4} lg={3} className='pr-2'>
+        <Col xs={12} sm={5} md={4} lg={3} className='pr-3'>
           <h2>Illustrations and Hand Drawings</h2>
 
-          <hr className='mr-2 my-3'/>
-
-          <b className='hide-xs'>
-            ACHIEVABLE SKILLS &nbsp;
-            <br className='hide-xs'/>
-            by DRAWINGS
-          </b>
-
-          <div className='sub show-xs'>
-            ACHIEVABLE SKILLS &nbsp;
-            <br className='hide-xs'/>
-            by DRAWINGS
+          <div className='show-xs'>
+            <hr className='my-3'/>
+            <b>ACHIEVABLE SKILLS by DRAWINGS</b>
+            <p>
+              1. Lower chance to loose Ideas<br/>
+              2. To develop prototypes quickly<br/>
+              3. Color Skills Up<br/>
+              4. Real freedom in your world
+            </p>
           </div>
 
-          <small>
-            1. Lower chance to loose Ideas<br/>
-            2. To developt prototypes quickly<br/>
-            3. Color Skills Up<br/>
-            4. Real freedom in your own world<br/>
-          </small>
+          <div className='hide-xs'>
+            <hr className='mr-2 my-3'/>
+            <div className='sub py-2'>
+              ACHIEVABLE SKILLS &nbsp;
+              <br/>
+              by DRAWINGS
+            </div>
+
+            <small>
+              1. Lower chance to loose Ideas<br/>
+              2. To developt prototypes quickly<br/>
+              3. Color Skills Up<br/>
+              4. Real freedom in your own world<br/>
+            </small>
+          </div>
         </Col>
 
         <Col xs={12} sm={7} md={8} lg={7} style={{paddingTop:'1.3rem'}}>
 
           <b>Between Logic and Art</b>
           <p>
-            Good architecture and Good user experienced design should be based on logical theory and data analysis. I beleive that to set a right goal in the beggining is one of the most important part of the project, but after setting strict rules, you can find out the artistic-freedom-spots easily.
+            Good architecture and Good user experienced design should be based on logical theory and data analysis. I believe that to set a logical goal in the beginning is one of the most important part of the project, but after setting strict rules, you can find out the artistic-freedom-spots easily.
           </p>
 
 
 
           <b>Personal Story</b>
           <p>
-            Until 17years old, all my future dream was being a illustrator. However, I couldn't allow to go art studio that I coldn't have
-            I collected animation, manga, illustraion on the magazine for fun   I got a best prize
-            design/ architecture university, I am trying not to loose artistic mind and drawing skills during a logical project.
-            <br/><br/><br/><br/>
+            It was miraculous that I won the best prize in the university art awards between more than thousands of art students who educated in the proper art studio.
+            Until 17 years old, all my future dream was being a painter, but I couldn't afford to go art studio at that time. Instead, I only collected some images on comic books or magazines and illustrated fictional characters from my dreamworld. <br/>
+            From this experience, I believe that if I want to achive new skills, practicing by yourself is not difficult. If you enjoy your work like a game, you will love to repeat practicing and finally anyone can reach the goal in the end.
+            <br/><br/>
+
           </p>
         </Col>
       </Row>
